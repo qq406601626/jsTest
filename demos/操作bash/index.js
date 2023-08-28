7ee57440d288c7499251a5de7f4ebf089c7bf060
guoshuai <406601626@qq.com>
2023-08-28 08:40:52 +0800

    修改

--- demos/操作bash/index.js
+++ demos/操作bash/index.js
@@ -1,2 +1,16 @@
-const { exec  } = require('node:child_process');
+const { spawn } = require('node:child_process');
+const ls = spawn('bash', [],{
 
+});
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
